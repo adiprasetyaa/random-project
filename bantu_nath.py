ea2a67943af1feb0d133b54af7265165ad8673ff
atyaa <88155210+adiprasetyaa@users.noreply.github.com>
2023-12-29 16:27:05 +0700

    Update bantu_nath.py

--- bantu_nath.py
+++ bantu_nath.py
@@ -1,136 +1,122 @@
-def biaya_lauk():
-    sum_ = 0
-
-    belilauk = input("Apakah anda ingin membeli lauk? (y/n) : ")
-
-    if belilauk.lower() == 'y':
-        while True:
-            biaya_lauk = [8000, 12000, 6000, 3000, 15000]
-
-            kodelauk = int(input("\nMasukkan kode lauk yang anda pilih : "))
-            jumlah = int(input("Masukkan jumlah lauk yang dipilih : "))
-
-            print(f"Harga Lauk = {biaya_lauk[kodelauk - 1] * jumlah}")
-
-            sum_ += biaya_lauk[kodelauk - 1] * jumlah
-
-            answer = input("Apakah anda ingin menambah lauk lagi? (y/n) : ")
-
-            if answer.lower() == 'y':
-                continue
-            elif answer.lower() == 'n':
-                break
-            else:
-                print("Kode yang anda masukkan tidak valid!")
-
-    print(f"TOTAL BIAYA LAUK : {sum_}")
-    return sum_
-
-
-def biaya_sayur():
-    sum_ = 0
-
-    belisayur = input("\nApakah anda ingin membeli sayur? (y/n) : ")
-
-    if belisayur.lower() == 'y':
-        while True:
-            biaya_sayur = [3000, 5000, 3000, 4000, 3000]
-
-            kodesayur = int(input("\nMasukkan kode sayur yang anda pilih : "))
-            jumlah = int(input("Masukkan jumlah porsi sayur yang dipilih : "))
-
-            print(f"Harga Sayur = {biaya_sayur[kodesayur - 1] * jumlah}")
-
-            sum_ += biaya_sayur[kodesayur - 1] * jumlah
-
-            answer = input("Apakah anda ingin menambah sayur lagi? (y/n) : ")
-
-            if answer.lower() == 'y':
-                continue
-            elif answer.lower() == 'n':
-                break
-            else:
-                print("Kode yang anda masukkan tidak valid!")
-
-    print(f"TOTAL BIAYA SAYUR : {sum_}")
-    return sum_
-
-
-def biaya_minum():
-    sum_ = 0
-
-    beliminum = input("\nApakah anda ingin membeli minum? (y/n) : ")
-
-    if beliminum.lower() == 'y':
-        while True:
-            biaya_minum = [4000, 5000, 3000]
-
-            kodeminuman = int(input("\nMasukkan kode minuman yang anda pilih : "))
-            jumlah = int(input("Masukkan jumlah minuman yang dipilih : "))
-
-            print(f"Harga Minuman = {biaya_minum[kodeminuman - 1] * jumlah}")
-
-            sum_ += biaya_minum[kodeminuman - 1] * jumlah
-
-            answer = input("Apakah anda ingin menambah minuman lagi? (y/n) : ")
-
-            if answer.lower() == 'y':
-                continue
-            elif answer.lower() == 'n':
-                break
-            else:
-                print("Kode yang anda masukkan tidak valid!")
-
-    print(f"TOTAL BIAYA MINUMAN : {sum_}")
-    return sum_
-
-
 def main():
-    print("====================================================")
-    print("=                                                  =")
-    print("=              WELCOME TO WARTEG MODERN            =")
-    print("=                                                  =")
-    print("====================================================")
+    print("MENU")
 
-    # Initialization of side dishes menu
-    lauk = ["Ayam goreng", "Ayam Bakar", "Lele Goreng", "Telur Dadar", "Rendang"]
+    print("\nAPPETIZER")
+    appetizer = ["Nachos", "Bread", "Chips"]
+    hargaappetizer = [27000, 23000, 25000]
+    for i, (item, price) in enumerate(zip(appetizer, hargaappetizer), 1):
+        print(f"[{i}] {item} : {price}")
 
-    print("---- PILIHAN LAUK ----")
-    for i, item in enumerate(lauk, 1):
-        print(f"[{i}] : {item}")
+    print("\nMAIN COURSE")
+    main_course = ["Aglio Olio", "Fetuccine Carbonara", "Nasi Goreng", "Chicken Mentai Rice", "Salmon Mentai Rice"]
+    hargamain = [41000, 41000, 37000, 40000, 50000]
+    for i, (item, price) in enumerate(zip(main_course, hargamain), 1):
+        print(f"[{i}] {item} : {price}")
 
-    # Initialization of vegetables menu
-    sayur = ["Sayur Bayam", "Sayur Sop", "Sayur Lodeh", "Sayur Labu", "Sayur Kangkung"]
+    print("\nSIDE DISH")
+    side_dish = ["French Fries", "Sausage", "Chicken Karage"]
+    hargaside = [27000, 30000, 33000]
+    for i, (item, price) in enumerate(zip(side_dish, hargaside), 1):
+        print(f"[{i}] {item} : {price}")
 
-    print("\n----  PILIHAN SAYUR ----")
-    for i, item in enumerate(sayur, 1):
-        print(f"[{i}] : {item}")
+    print("\nDESSERT")
+    dessert = ["Mochi Ice Cream", "Caramel Custard"]
+    hargadessert = [21000, 28000]
+    for i, (item, price) in enumerate(zip(dessert, hargadessert), 1):
+        print(f"[{i}] {item} : {price}")
 
-    # Initialization of drinks menu
-    minum = ["Es Teh", "Es Jeruk", "Air mineral"]
+    print("\nBEVERAGE")
+    beverage = ["Ice Tea", "Artisan Tea", "Americano", "Coffee Latte", "Mineral Water"]
+    hargabeverage = [18000, 28000, 21000, 27000, 12000]
+    for i, (item, price) in enumerate(zip(beverage, hargabeverage), 1):
+        print(f"[{i}] {item} : {price}")
 
-    print("\n---- PILIHAN MINUMAN ----")
-    for i, item in enumerate(minum, 1):
-        print(f"[{i}] : {item}")
+    def pesan_appetizer():
+        jumlah = 0
+        beliappetizer = input("Apakah ada appetizer yang ingin dipesan? (y/n) ")
+        if beliappetizer.lower() == "y":
+            while True:
+                kodeappetizer = int(input("Masukkan kode appetizer: "))
+                qty = int(input("Masukkan jumlah: "))
+                print(f"Total harga item = {hargaappetizer[kodeappetizer - 1] * qty}")
+                jumlah += hargaappetizer[kodeappetizer - 1] * qty
+                tambah = input("Apakah ada appetizer lain yang ingin dipesan? (y/n) ")
+                if tambah.lower() == "y":
+                    continue
+                else:
+                    break
+            print(f"Total harga appetizer = {jumlah}")
+            return jumlah
 
-    print("\nINTRUKSI USER\n[kode] : pilihan\n")
+    def pesan_main():
+        jumlah = 0
+        belimain = input("Apakah ada main course yang ingin dipesan? (y/n) ")
+        if belimain.lower() == "y":
+            while True:
+                kodemain = int(input("Masukkan kode main course: "))
+                qty = int(input("Masukkan jumlah: "))
+                print(f"Total harga item = {hargamain[kodemain - 1] * qty}")
+                jumlah += hargamain[kodemain - 1] * qty
+                tambah = input("Apakah ada main course lain yang ingin dipesan? (y/n) ")
+                if tambah.lower() == "y":
+                    continue
+                else:
+                    break
+            print(f"Total harga main course = {jumlah}")
+            return jumlah
 
-    total_pembayaran = biaya_lauk() + biaya_sayur() + biaya_minum()  # Calculate total payment
+    def pesan_side():
+        jumlah = 0
+        beliside = input("Apakah ada side dish yang ingin dipesan? (y/n) ")
+        if beliside.lower() == "y":
+            while True:
+                kodeside = int(input("Masukkan kode side dish: "))
+                qty = int(input("Masukkan jumlah: "))
+                print(f"Total harga item = {hargaside[kodeside - 1] * qty}")
+                jumlah += hargaside[kodeside - 1] * qty
+                tambah = input("Apakah ada side dish lain yang ingin dipesan? (y/n) ")
+                if tambah.lower() == "y":
+                    continue
+                else:
+                    break
+            print(f"Total harga side dish = {jumlah}")
+            return jumlah
 
-    print(f"\n\nTOTAL PEMBAYARAN : {total_pembayaran}")
+    def pesan_dessert():
+        jumlah = 0
+        belidessert = input("Apakah ada dessert yang ingin dipesan? (y/n) ")
+        if belidessert.lower() == "y":
+            while True:
+                kodedessert = int(input("Masukkan kode dessert: "))
+                qty = int(input("Masukkan jumlah: "))
+                print(f"Total harga item = {hargadessert[kodedessert - 1] * qty}")
+                jumlah += hargadessert[kodedessert - 1] * qty
+                tambah = input("Apakah ada dessert lain yang ingin dipesan? (y/n) ")
+                if tambah.lower() == "y":
+                    continue
+                else:
+                    break
+            print(f"Total harga dessert = {jumlah}")
+            return jumlah
 
-    bayar = int(input("TOTAL UANG YANG DIBAYARKAN : "))  # Input total money paid
+    def pesan_beverage():
+        jumlah = 0
+        belibeverage = input("Apakah ada beverage yang ingin dipesan? (y/n) ")
+        if belibeverage.lower() == "y":
+            while True:
+                kodebeverage = int(input("Masukkan kode beverage: "))
+                qty = int(input("Masukkan jumlah: "))
+                print(f"Total harga item = {hargabeverage[kodebeverage - 1] * qty}")
+                jumlah += hargabeverage[kodebeverage - 1] * qty
+                tambah = input("Apakah ada beverage lain yang ingin dipesan? (y/n) ")
+                if tambah.lower() == "y":
+                    continue
+                else:
+                    break
+            print(f"Total harga beverage = {jumlah}")
+            return jumlah
 
-    kembalian = bayar - total_pembayaran  # Calculate money changes
+    totalbayar = pesan_appetizer() + pesan_main() + pesan_side() + pesan_dessert() + pesan_beverage()
+    print("Total yang harus dibayar: ", totalbayar)
 
-    print(f"\nTOTAL SISA UANG DARI PEMBAYARAN : {kembalian}")
-
-    print("====================================================")
-    print("=                                                  =")
-    print("=        TERIMA KASIH DAN SELAMAT MENIKMATI        =")
-    print("=                                                  =")
-    print("====================================================")
-
-
-if __name__ == "__main__":
-    main()
+main()
